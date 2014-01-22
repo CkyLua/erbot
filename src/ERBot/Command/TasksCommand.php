4f35c9a44a148bd22f6876d2342c710ef47693e5
stil <przemekryciuk@gmail.com>
2014-01-22 22:00:10 +0100

    You can choose training grounds in TasksCommand

--- src/ERBot/Command/TasksCommand.php
+++ src/ERBot/Command/TasksCommand.php
@@ -14,17 +14,49 @@ class TasksCommand extends Command
     {
         $this
             ->setName('tasks')
-            ->setDescription('Complete daily tasks.');
+            ->setDescription('Complete daily tasks.')
+            ->addOption(
+                'train2',
+                null,
+                InputOption::VALUE_NONE,
+                'Train in Climbing Center (0.19g)'
+            )
+            ->addOption(
+                'train3',
+                null,
+                InputOption::VALUE_NONE,
+                'Train in Shooting Range (0.89g)'
+            )
+            ->addOption(
+                'train4',
+                null,
+                InputOption::VALUE_NONE,
+                'Train in Special Forces Center (1.79g)'
+            );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $client = $this->getApplication()->erpkClient;
         $mgmt = new ManagementModule($client);
-        $mgmt->workAsEmployee();
+
+        $output->write('Working... ');
+        $result = $mgmt->workAsEmployee();
+        $output->writeln($result['status'] ? 'OK' : 'error ('.$result['message'].')');
         sleep(5);
-        $mgmt->train(true, true, true, true);
+
+        $output->write('Training... ');
+        $result = $mgmt->train(
+            true,
+            $input->getOption('train2'),
+            $input->getOption('train3'),
+            $input->getOption('train4')
+        );
+        $output->writeln($result['status'] ? 'OK' : 'error ('.$result['message'].')');
+
         sleep(5);
-        $mgmt->getDailyTasksReward();
+        $output->write('Getting daily rewards... ');
+        $result = $mgmt->getDailyTasksReward();
+        $output->writeln($result['message']);
     }
 }
