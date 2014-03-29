e89d028aec5254510253941aac3afd2fdacb276a
stil <przemekryciuk@gmail.com>
2014-03-29 11:17:50 +0100

    Flattened fighter algorithm

--- src/ERBot/Command/FighterCommand.php
+++ src/ERBot/Command/FighterCommand.php
@@ -26,6 +26,7 @@ class FighterCommand extends Command implements EventSubscriberInterface
     {
         return [
             'startup'             => ['onStartup'],
+            'exit'                => ['onExit'],
             'fight.ready'         => ['onFightReady'],
             'fight.BATTLE_WON'    => ['onBattleWon'],
             'fight.ZONE_INACTIVE' => ['onZoneInactive'],
@@ -41,35 +42,6 @@ class FighterCommand extends Command implements EventSubscriberInterface
         $this
             ->setName('fighter')
             ->setDescription('Burn energy in automatically choosen campaign.');
-    }
-
-    protected function addCampaigns($campaigns)
-    {
-        $this->campaignsAvailable = array_merge($this->campaignsAvailable, $campaigns);
-    }
-
-    protected function chooseCampaign($event)
-    {
-        while ($id = array_shift($this->campaignsAvailable)) {
-            $event->output->writeln('Getting info about campaign '.$id.'... ');
-            $campaign = $event->mil->getCampaign($id);
-            $event->output->writeln('Region: '.$campaign->getRegion()->getName());
-            $event->output->writeln('Resistance war: '.($campaign->isResistance() ? 'yes' : 'no'));
-            $event->output->writeln('Getting additional statistics... ');
-            $campaignStats = $event->mil->getCampaignStats($campaign);
-            if (!$campaignStats['is_finished']) {
-                $result = $event->mil->changeWeapon($campaign->getId(), $this->weaponQuality);
-                if ($result) {
-                    return $campaign;
-                } else {
-                    throw new Exception('You don\'t have Q'.$this->weaponQuality.' weapon');
-                }
-            } else {
-                $event->output->writeln('Already ended');
-            }
-        }
-        $event->output->writeln('No campaigns available, terminating.');
-        exit;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
@@ -95,6 +67,39 @@ class FighterCommand extends Command implements EventSubscriberInterface
         $event->output->writeln('<comment>done</comment>');
     }
 
+    protected function addCampaigns($campaigns)
+    {
+        $this->campaignsAvailable = array_merge($this->campaignsAvailable, $campaigns);
+    }
+
+    protected function chooseCampaign($event)
+    {
+        while ($id = array_shift($this->campaignsAvailable)) {
+            $event->output->writeln('Getting info about campaign '.$id.'... ');
+            $campaign = $event->mil->getCampaign($id);
+            $event->output->writeln('Region: '.$campaign->getRegion()->getName());
+            $event->output->writeln('Resistance war: '.($campaign->isResistance() ? 'yes' : 'no'));
+            $event->output->writeln('Getting additional statistics... ');
+            $campaignStats = $event->mil->getCampaignStats($campaign);
+            if (!$campaignStats['is_finished']) {
+                $result = $event->mil->changeWeapon($campaign->getId(), $this->weaponQuality);
+
+                if ($result) {
+                    $event->output->writeln('Using Q'.$this->weaponQuality.' weapons.');
+                } else {
+                    $event->output->writeln('Using no weapons to fight.');
+                    $result = $event->mil->changeWeapon($campaign->getId(), 0);
+                }
+
+                return $campaign;
+            } else {
+                $event->output->writeln('Already ended');
+            }
+        }
+        $event->output->writeln('No campaigns available, terminating.');
+        exit;
+    }
+
     public function onStartup(Event $event, $eventName, $dispatcher)
     {
         $event->output->writeln(
@@ -111,17 +116,25 @@ class FighterCommand extends Command implements EventSubscriberInterface
 
             $event->output->writeln('loaded');
             $event->currentCampaign = $this->chooseCampaign($event);
-            $dispatcher->dispatch('fight.ready', $event);
+
+            while (true) {
+                $dispatcher->dispatch('fight.ready', $event);
+                $this->humanSleep($event);
+            }
         } catch (\Exception $e) {
-            $event->output->writeln('<error>'.(string)$e.'</error>');
+            $event->output->writeln('<error>'.$e->getMessage().'</error>');
         }
+    }
+
+    public function onExit(Event $event, $eventName, $dispatcher)
+    {
+        $event->output->write('<comment>Done.</comment>');
+        exit;
     }
 
     public function onFightReady(Event $event, $eventName, $dispatcher)
     {
         $event->output->writeln(str_repeat('-', 60));
-        $this->humanSleep($event);
-        
         $event->output->writeln('<comment>Fighting... </comment>');
         $event->fightResult = $event->mil->fight($event->currentCampaign);
 
@@ -140,26 +153,22 @@ class FighterCommand extends Command implements EventSubscriberInterface
     public function onBattleWon(Event $event, $eventName, $dispatcher)
     {
         $event->output->writeln('<comment>Campaign ended.</comment>');
-
         $event->currentCampaign = $this->chooseCampaign($event);
-        $dispatcher->dispatch('fight.ready', $event);
     }
 
     public function onZoneInactive(Event $event, $eventName, $dispatcher)
     {
         $event->output->writeln('<comment>Waiting for the next round!</comment>');
         sleep(30);
-        $dispatcher->dispatch('fight.ready', $event);
     }
 
     public function onLowEnergy(Event $event, $eventName, $dispatcher)
     {
         $this->eat($event);
 
-        if ($event->hp >= $this->minimumEnergy) {
-            $dispatcher->dispatch('fight.ready', $event);
-        } else {
+        if ($event->hp < $this->minimumEnergy) {
             $event->output->writeln('<comment>Not enough energy to fight, terminating!</comment>');
+            $dispatcher->dispatch('exit', $event);
         }
     }
 
@@ -174,18 +183,15 @@ class FighterCommand extends Command implements EventSubscriberInterface
             'you have '.round($event->hp).' HP and '.round($event->hpRecoverable).' HP recoverable</info>'
         );
 
-        if ($event->hp >= $this->minimumEnergy) {
-            $dispatcher->dispatch('fight.ready', $event);
-        } else {
+        if ($event->hp < $this->minimumEnergy) {
             if ($event->hpRecoverable > 0) {
                 $this->eat($event);
                 if ($event->hp < $this->minimumEnergy) {
                     $event->output->writeln('<info>Not enough energy to fight, terminating!</info>');
-                } else {
-                    $dispatcher->dispatch('fight.ready', $event);
                 }
             } else {
                 $event->output->writeln('<info>Success, finishing!</info>');
+                $dispatcher->dispatch('exit', $event);
             }
         }
     }
@@ -194,7 +200,6 @@ class FighterCommand extends Command implements EventSubscriberInterface
     {
         $event->output->writeln('<comment>Too fast, waiting a moment...</comment>');
         sleep(30);
-        $dispatcher->dispatch('fight.ready', $event);
     }
 
     protected function humanSleep($event)
@@ -208,11 +213,13 @@ class FighterCommand extends Command implements EventSubscriberInterface
     {
         $event->output->write('<comment>Eating... </comment>');
         
-        $eatResult = $event->mgmt->eat();
-        var_dump($eatResult);
-
-        $event->hp = $eatResult['health'];
-        $event->hpRecoverable = $eatResult['food_remaining'];
+        $attempts = 5;
+        while ($event->hpRecoverable > 0 && $attempts > 0) {
+            $eatResult = $event->mgmt->eat();
+            $event->hp = $eatResult['health'];
+            $event->hpRecoverable = $eatResult['food_remaining'];
+            $attempts--;
+        }
 
         $event->output->writeln(
             '<comment>now you have '.$event->hp.' HP and '.$event->hpRecoverable.' HP recoverable<comment>'
